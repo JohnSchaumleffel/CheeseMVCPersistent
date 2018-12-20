8d7083d4595603e64fd11226310575ed0f95fa50
johnschaumleffel <johnschaumleffel@gmail.com>
2018-12-20 01:22:54 -0600

    Setup category controller, model, viewmodel. Created views for categories. Added link to shared layout.

---
+++ src/CheeseMVC/ViewModels/AddCategoryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddCategoryViewModel
+    {
+        [Required]
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+
+        public AddCategoryViewModel() { }
+
+    }
+}
